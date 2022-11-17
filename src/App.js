f3e23ae4fc454cf5b6d60248041f4b823480337d
zaidkhan1123 <zk117854@gmail.com>
2022-11-17 18:59:46 +0500

    gggg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import axios from "axios"
 import { useState } from "react";
-import Modal from "./modal"
+import Modal from "./Modal"
 import './App.css';
 import img from "./images/edit.png"
 function App() {
@@ -9,7 +9,7 @@ function App() {
 const [data,setData]= useState([])
 
 useEffect(()=>{
-let URL = "https://reqres.in/api/users?page=2"
+let URL = "https://reqres.in/api/users?page=2 "
 axios.get(URL).then((response)=>{
 console.log(response.data);
 if(response.data.data){
@@ -29,7 +29,7 @@ return   i
   <>
       <Modal/>
   {/* {modal && <Modal closemodal={setmodal}  /> } */}
- <div className="btn-div" ><button  className="btnn" data-bs-toggle="modal" data-bs-target="#exampleModal">Create User</button> </div> 
+ <div className="btn-div" ><button  className="btnn" data-toggle="modal" data-target="#exampleModal">Create User</button> </div> 
   <div className="App">
    {data.map((elem)=>{
    return <> <div className="User" > {<img className="avatar" src={elem.avatar} />} <div className="userchild">{elem.first_name}</div> 
--- src/modal.jsx
+++ src/modal.jsx
@@ -1,26 +1,77 @@
 
-import React from 'react'
+
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 function Modal() {
+  const [first_name,setfirst_name]=useState("")
+  const [email,setemail]=useState("")
+
+  function postData (){
+  const postdata = {
+    first_name,
+    email
+  }
+ Axios.post("https://reqres.in/api/users?page=2/posts",
+  postdata
+  ).then(res => console.log("Posting Data",res)).catch((err)=>{
+  console.log(err);
+  })
+
+  }
+
+
+
   return (
-    
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    <>
+  
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex={-1}
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Create New User
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div className="modal-body text-center my-3  ">
+            <form id="form">
+          <label htmlFor="text">Name</label><input className='m-3' type="text" id='text' placeholder='First Name' value={first_name} onChange={(e)=>{setfirst_name(e.target.value)}} /><br />
+           <label htmlFor="Email">Email</label><input className='m-3' type="Email" id='Email' placeholder='Email' value={email} onChange={(e)=>{setemail(e.target.value)}} />
+           </form>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="submit" htmlFor="form" onClick={postData} className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+  </>
+  
+
   )
 }
 
